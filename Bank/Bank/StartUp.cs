5354d1eda9dbd7f0e7f9463e84b3f7cde74cf562
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-10-03 10:48:00 +0300

    Add files via upload

---
+++ Bank/Bank/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Bank
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] c = Console.ReadLine().Split(',');
+            BankAccount cus = new BankAccount(c[0], decimal.Parse(c[1]));
+            int input = int.Parse(Console.ReadLine());
+            
+            for(int i = 0; i < input; i++)
+            {
+                string[] n = Console.ReadLine().Split(": ");
+                if (n[0] == "Deposit")
+                {
+                    cus.DepositTransaction(decimal.Parse(n[1]));
+                }
+                else if (n[0] == "Withdrawal")
+                {
+                    cus.WithdrawalTransaction(decimal.Parse(n[1]));
+                }
+            }
+            Console.WriteLine($"Dear {cus.CustomerName}, your balance is: {cus.Balance}");
+        }
+    }
+}
---
+++ Bank/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        public string CustomerName { get; set; }
+        public decimal Balance { get; set; }
+
+        public BankAccount(string customerName, decimal balance)
+        {
+            CustomerName = customerName;
+            Balance = balance;
+        }
+        public decimal DepositTransaction(decimal deposits)
+        {
+            Balance += deposits;
+            return Balance;        }
+        public decimal WithdrawalTransaction(decimal cash)
+        {
+            Balance -= cash;
+            return Balance; 
+        }
+        public override string ToString()
+        {
+            return ("Dear " + CustomerName + ", your balance is: " + Balance);
+        }
+    }
+}
